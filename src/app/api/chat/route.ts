75a3b44af348ccd928300f7880353247f033900e
ju78832 <junedgreat89@gmail.com>
2025-02-03 11:07:18 +0530

    Create register endpoint

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,119 @@
+import { NextRequest, NextResponse } from "next/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { prisma } from "@/helper";
+import { createClient } from "redis";
+import { RedisClientType } from "@redis/client";
+
+let client: RedisClientType;
+let cacheData: any;
+
+(async () => {
+  client = createClient();
+  client.on("error", (err) => console.log("Redis Client Error", err));
+
+  await client.connect();
+})();
+
+export async function POST(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const email = searchParams.get("email");
+
+  try {
+    const body = await req.json();
+    const { message } = body;
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY || "");
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email!,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    await prisma.chat.create({
+      data: {
+        content: message,
+        role: "user",
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+
+    const chat = model.startChat({
+      history: [
+        {
+          role: "user",
+          parts: [{ text: "Hello" }],
+        },
+        {
+          role: "model",
+          parts: [{ text: "Great to meet you. What would you like to know?" }],
+        },
+      ],
+    });
+
+    const result = await chat.sendMessage(message);
+
+    const chatResponse = await prisma.chat.create({
+      data: {
+        content: result.response.text(),
+        role: "assistance",
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+
+    const data = await prisma.chat.findMany({
+      where: {
+        user: {
+          email: email!,
+        },
+      },
+    });
+
+    cacheData = await client.set("chat", JSON.stringify(data));
+
+    return NextResponse.json(
+      { message: chatResponse, email: email },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const email = searchParams.get("email");
+
+  try {
+    cacheData = await client.get("chat");
+    if (cacheData) {
+      return NextResponse.json({ chat: cacheData }, { status: 200 });
+    }
+
+    const chat = await prisma.chat.findMany({
+      where: {
+        user: {
+          email: email!,
+        },
+      },
+    });
+
+    cacheData = await client.set("chat", JSON.stringify(chat));
+
+    return NextResponse.json({ chat: cacheData }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
---
+++ src/app/api/register/route.ts
@@ -0,0 +1,51 @@
+import { prisma } from "@/helper";
+import { sendEmail } from "@/helper/sendemail";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  try {
+    const { name, email, password } = body;
+    const emailExists = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (emailExists) {
+      return new Response("Email already exists", { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const code = Math.ceil(Math.random() * 900000 + 100000);
+    const time = new Date(Date.now() + 5 * 60 * 1000);
+
+    const user = await prisma.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: hashedPassword,
+        verifyCode: code,
+        verifyCodeExpire: time,
+      },
+    });
+
+    const verifymail = await sendEmail(email, code);
+    if (!verifymail.success) {
+      return NextResponse.json(
+        { message: verifymail.message },
+        { status: 403 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: "User created Successfully. Make sure to verify your email.",
+        user: user,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: "Error creating user" }, { status: 500 });
+  }
+}
