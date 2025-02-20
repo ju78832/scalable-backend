b5480c480f2acd978e9a3f589930fea60e884b15
ju78832 <junedgreat89@gmail.com>
2025-02-20 19:10:49 +0530

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,23 @@
-import { getServerSession } from "next-auth";
+import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 export { default } from "next-auth/middleware";
 
 export const config = { matcher: ["/sign-in", "/sign-up", "/chat"] };
 
 export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+
+  if (
+    token &&
+    (req.nextUrl.pathname === "/sign-in" ||
+      req.nextUrl.pathname === "/sign-up" ||
+      req.nextUrl.pathname === "/")
+  ) {
+    return NextResponse.redirect(new URL("/chat", req.url));
+  }
+
+  if (!token && req.nextUrl.pathname === "/chat") {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
+  }
   return NextResponse.next();
 }
